0c3f7ba6b92e89a5b9560158575a7f82021bdc70
Sachin <sachin.ec23@gmail.com>
2025-05-22 15:00:45 +0530

    Implemented threading feature

--- file1.py
+++ file1.py
@@ -1,1 +1,12 @@
+import threading
+import time
 print("Starting in file1")
+
+def printTem():
+    print("Started")
+    time.sleep(1)
+    print("Done")
+
+thread1=threading.Thread(target=printTem)
+
+thread1.start()
