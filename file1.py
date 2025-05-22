5ca92190c2c3c43b15a5e9974ec46690441a5ba3
Sachin <sachin.ec23@gmail.com>
2025-05-22 16:40:16 +0530

    added list feature

--- file1.py
+++ file1.py
@@ -1,12 +1,4 @@
 import threading
-import time
-print("Starting in file1")
 
-def printTem():
-    print("Begin")
-    time.sleep(1)
-    print("Finish")
-
-thread2=threading.Thread(target=printTem)
-
-thread2.start()
+my_list=[x for x in range(10)]
+print(my_list)
